59f37a5fdd47f4ba2c338c4eacc0a29c1a4d7767
Jon Connell <python@figsandfudge.com>
2022-12-11 21:12:33 +0000

    Version 0.7

--- custom_components/kingspan_watchman_sensit/const.py
+++ custom_components/kingspan_watchman_sensit/const.py
@@ -3,7 +3,7 @@
 NAME = "Kingspan Watchman SENSiT"
 DOMAIN = "kingspan_watchman_sensit"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "0.6"
+VERSION = "0.7"
 
 ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
 ISSUE_URL = "https://github.com/masaccio/ha-kingspan-watchman-sensit/issues"
