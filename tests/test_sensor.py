7b05b44e0216aee899e74c3eb39778d384d6f432
Jon Connell <python@figsandfudge.com>
2022-12-12 17:04:21 +0000

    Test various tank levels for coverage

--- tests/test_sensor.py
+++ tests/test_sensor.py
@@ -2,23 +2,19 @@
 import pytest
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 from custom_components.kingspan_watchman_sensit.const import DOMAIN
+from custom_components.kingspan_watchman_sensit import async_unload_entry
 
 from .const import MOCK_TANK_LEVEL, MOCK_TANK_CAPACITY, MOCK_TANK_LAST_READ
-from homeassistant.const import (
-    # ATTR_DEVICE_CLASS,
-    ATTR_ICON,
-    # ATTR_UNIT_OF_MEASUREMENT,
-    # STATE_UNAVAILABLE,
-)
+from homeassistant.const import ATTR_ICON
 
 
 @pytest.mark.asyncio
 async def test_sensor(hass, mock_sensor_client):
     """Test sensor."""
-    entry = MockConfigEntry(domain=DOMAIN, data={"name": "simple config"})
+    config_entry = MockConfigEntry(domain=DOMAIN, data={"name": "simple config"})
 
-    entry.add_to_hass(hass)
-    await hass.config_entries.async_setup(entry.entry_id)
+    config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(config_entry.entry_id)
     await hass.async_block_till_done()
 
     state = hass.states.get("sensor.oil_level")
@@ -40,3 +36,61 @@ async def test_sensor(hass, mock_sensor_client):
     assert state
     assert state.state == MOCK_TANK_LAST_READ.isoformat()
     assert state.attributes.get(ATTR_ICON) == "mdi:clock-outline"
+
+    assert await async_unload_entry(hass, config_entry)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("mock_sensor_client", [[MOCK_TANK_CAPACITY]], indirect=True)
+async def test_sensor_icon_full(hass, mock_sensor_client):
+    """Test sensor."""
+    config_entry = MockConfigEntry(domain=DOMAIN, data={"name": "simple config"})
+
+    config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    state = hass.states.get("sensor.oil_level")
+    assert state
+    assert state.state == str(MOCK_TANK_CAPACITY)
+    assert state.attributes.get(ATTR_ICON) == "mdi:gauge-full"
+
+    assert await async_unload_entry(hass, config_entry)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("mock_sensor_client", [[100]], indirect=True)
+async def test_sensor_icon_empty(hass, mock_sensor_client):
+    """Test sensor."""
+    config_entry = MockConfigEntry(domain=DOMAIN, data={"name": "simple config"})
+
+    config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    state = hass.states.get("sensor.oil_level")
+    assert state
+    assert state.state == str(100)
+    assert state.attributes.get(ATTR_ICON) == "mdi:gauge-empty"
+
+    assert await async_unload_entry(hass, config_entry)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "mock_sensor_client", [[MOCK_TANK_CAPACITY * 0.3]], indirect=True
+)
+async def test_sensor_icon_low(hass, mock_sensor_client):
+    """Test sensor."""
+    config_entry = MockConfigEntry(domain=DOMAIN, data={"name": "simple config"})
+
+    config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    state = hass.states.get("sensor.oil_level")
+    assert state
+    assert state.state == str(MOCK_TANK_CAPACITY * 0.3)
+    assert state.attributes.get(ATTR_ICON) == "mdi:gauge-low"
+
+    assert await async_unload_entry(hass, config_entry)
