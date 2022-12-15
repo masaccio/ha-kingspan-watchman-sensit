fe19662c03604913dd64e25c3ce7f0523612e26d
Jon Connell <python@figsandfudge.com>
2022-12-15 10:23:33 +0000

    Ensure only one device is present

--- custom_components/kingspan_watchman_sensit/const.py
+++ custom_components/kingspan_watchman_sensit/const.py
@@ -3,7 +3,6 @@
 NAME = "Kingspan Watchman SENSiT"
 DOMAIN = "kingspan_watchman_sensit"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "0.7"
 
 ATTRIBUTION = "Data provided by https://www.connectsensor.com/"
 ISSUE_URL = "https://github.com/masaccio/ha-kingspan-watchman-sensit/issues"
