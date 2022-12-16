451f75ae10d20e4e2a9945e82b4f0d6de87fe784
Jon Connell <python@figsandfudge.com>
2022-12-16 11:00:05 +0000

    Forcast empty sensor renamed, changed to days

--- local/deploy.sh
+++ local/deploy.sh
@@ -1,2 +1,10 @@
 #! /bin/bash
-scp custom_components/kingspan_watchman_sensit/*.py custom_components/kingspan_watchman_sensit/manifest.json hass:/config/custom_components/kingspan_watchman_sensit
+REMOTE_HOST="hass"
+SRCDIR="custom_components/kingspan_watchman_sensit"
+for file in $SRCDIR/*.py $SRCDIR/manifest.json; do
+  echo $(tput setaf 2)Copying $file$(tput sgr0)
+  scp -q $file $REMOTE_HOST:/config/$SRCDIR
+done
+echo $(tput setaf 2)"Restarting HA Core"$(tput sgr0)
+ssh $REMOTE_HOST "source /etc/profile.d/homeassistant.sh && ha core restart"
+echo $(tput setaf 2)"Done"$(tput sgr0)
