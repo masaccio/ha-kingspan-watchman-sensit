a0e46f8d0de06f23bd26693091cd4a715994a84f
Jon Connell <python@figsandfudge.com>
2023-09-17 15:55:58 +0100

    Test history windows

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,7 @@
 """Tests for Kingspan Watchman SENSiT api."""
 import asyncio
-from datetime import timezone, datetime
+import random
+from datetime import datetime, timezone
 
 import pandas as pd
 import pytest
@@ -20,7 +21,9 @@ from .const import (
 
 async def test_api(hass, mock_sensor_client, mocker, caplog):
     """Test API calls."""
-    api = SENSiTApiClient("test", "test")
+    random.seed(999)
+
+    api = SENSiTApiClient("test", "test", 14)
     tank_data = await api.async_get_data()
     assert tank_data[0].level == MOCK_TANK_LEVEL
     assert tank_data[0].serial_number == MOCK_TANK_SERIAL_NUMBER
@@ -28,12 +31,13 @@ async def test_api(hass, mock_sensor_client, mocker, caplog):
     assert tank_data[0].name == MOCK_TANK_NAME
     assert tank_data[0].capacity == MOCK_TANK_CAPACITY
     history = pd.DataFrame(tank_data[0].history)
+    print(history)
     local_tzinfo = datetime.now(timezone.utc).astimezone().tzinfo
     assert tank_data[0].last_read == history.iloc[-1].reading_date.replace(
         tzinfo=local_tzinfo
     )
-    assert tank_data[0].usage_rate == 80.0
-    assert tank_data[0].forecast_empty == 15
+    assert int(tank_data[0].usage_rate) == 100
+    assert tank_data[0].forecast_empty == 10
 
     caplog.clear()
     mocker.patch(MOCK_GET_DATA_METHOD, side_effect=asyncio.TimeoutError)
@@ -52,3 +56,18 @@ async def test_api(hass, mock_sensor_client, mocker, caplog):
     _ = await api.async_get_data()
     assert len(caplog.record_tuples) == 1
     assert "Unhandled error" in caplog.record_tuples[0][2]
+
+
+async def test_api_filtering(hass, mock_sensor_client, mocker):
+    """Test API calls."""
+    random.seed(999)
+
+    api = SENSiTApiClient("test", "test", 10)
+    tank_data = await api.async_get_data()
+    history = pd.DataFrame(tank_data[0].history)
+    assert int(tank_data[0].usage_rate) == 102
+
+    api = SENSiTApiClient("test", "test", 5)
+    tank_data = await api.async_get_data()
+    history = pd.DataFrame(tank_data[0].history)
+    assert int(tank_data[0].usage_rate) == 115
