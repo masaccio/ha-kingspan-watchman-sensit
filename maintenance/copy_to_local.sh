ec29d16ab026afb5de44a3097988da10c17be518
Jon Connell <python@figsandfudge.com>
2023-09-17 12:38:20 +0100

    Copy translations

--- maintenance/copy_to_local.sh
+++ maintenance/copy_to_local.sh
@@ -5,6 +5,10 @@ for file in $SRCDIR/*.py $SRCDIR/manifest.json; do
   echo $(tput setaf 2)Copying $file$(tput sgr0)
   scp -q $file $REMOTE_HOST:/config/$SRCDIR
 done
+
+echo $(tput setaf 2)Copying $SRCDIR/translations/en.json$(tput sgr0)
+scp -q $SRCDIR/translations/en.json $REMOTE_HOST:/config/$SRCDIR/translations
+
 echo $(tput setaf 2)"Restarting HA Core"$(tput sgr0)
 ssh $REMOTE_HOST "source /etc/profile.d/homeassistant.sh && ha core restart"
 echo $(tput setaf 2)"Done"$(tput sgr0)
